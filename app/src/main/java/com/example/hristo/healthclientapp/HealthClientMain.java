f404329245ba9adfe6fa0e90dc15aff2fad224ce
Hristo <crizov@hotmail.com>
2018-08-03 15:23:18 +1000

    Initial commit

---
+++ app/src/main/java/com/example/hristo/healthclientapp/HealthClientMain.java
@@ -0,0 +1,13 @@
+package com.example.hristo.healthclientapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HealthClientMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health_client_main);
+    }
+}
