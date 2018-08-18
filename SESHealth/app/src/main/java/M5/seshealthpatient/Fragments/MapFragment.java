300364549606bd34628b9c8070105761498c3e00
Hayden Crain <haydencrain@optusnet.com.au>
2018-08-18 14:44:56 +1000

    add basic mapview setup (not working yet)

--- SESHealth/app/src/main/java/M5/seshealthpatient/Fragments/MapFragment.java
+++ SESHealth/app/src/main/java/M5/seshealthpatient/Fragments/MapFragment.java
@@ -7,13 +7,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+
 import M5.seshealthpatient.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
 
+    MapView mMapView;
+    private GoogleMap _googleMap;
 
     public MapFragment() {
         // Required empty public constructor
@@ -24,7 +33,51 @@ public class MapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        mMapView = view.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.getMapAsync(this);
+        mMapView.onResume();
+
+        return view;
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        _googleMap = googleMap;
+        _googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(43.1, -87.9), 10);
+        _googleMap.moveCamera(cameraUpdate);
+    }
+
+
+    @Override
+    public void onResume() {
+        mMapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+
 
 }
