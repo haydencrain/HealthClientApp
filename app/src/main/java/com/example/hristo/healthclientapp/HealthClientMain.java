9addb7a2d621b6b094a53dc306d4555fd22c98f2
Hubert <hubertchau97@gmail.com>
2018-08-21 10:45:01 +1000

    Add Login

--- app/src/main/java/com/example/hristo/healthclientapp/HealthClientMain.java
+++ app/src/main/java/com/example/hristo/healthclientapp/HealthClientMain.java
@@ -1,13 +1,37 @@
 package com.example.hristo.healthclientapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HealthClientMain extends AppCompatActivity {
 
+        private Button patProfile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health_client_main);
+        patProfile = (Button) findViewById(R.id.patProfile);
+        patProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HealthClientMain.this, ProfileActivity.class));
+            }
+        });
     }
+
+
+
 }
