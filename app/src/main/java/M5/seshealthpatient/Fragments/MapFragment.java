5b879ad7f0a2660bceb86bb72eac89509948003a
Hayden Crain <haydencrain@optusnet.com.au>
2018-08-21 18:13:18 +1000

    add map view, request permissions for location when required

--- app/src/main/java/M5/seshealthpatient/Fragments/MapFragment.java
+++ app/src/main/java/M5/seshealthpatient/Fragments/MapFragment.java
@@ -1,11 +1,21 @@
 package M5.seshealthpatient.Fragments;
 
 
+import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.os.Bundle;
+import android.Manifest;
 import android.app.Fragment;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.location.Location;
+import android.location.LocationManager;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -49,8 +59,25 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         _googleMap = googleMap;
         _googleMap.getUiSettings().setMyLocationButtonEnabled(false);
 
+        double lat = -33.86, lon = 151.2;
 
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(43.1, -87.9), 10);
+        if (checkLocationPermission()) {
+            if (ContextCompat.checkSelfPermission(getActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+
+                //Request location updates:
+                googleMap.setMyLocationEnabled(true);
+
+                LocationManager mng = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+                Location location = mng.getLastKnownLocation(mng.getBestProvider(new Criteria(), false));
+
+                lat = location.getLatitude();
+                lon = location.getLongitude();
+            }
+        }
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lon), 15);
         _googleMap.moveCamera(cameraUpdate);
     }
 
@@ -77,6 +104,77 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     public void onLowMemory() {
         super.onLowMemory();
         mMapView.onLowMemory();
+    }
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(getActivity())
+                        .setTitle("")
+                        .setMessage("")
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(getActivity(),new String[]
+                                    {Manifest.permission.ACCESS_FINE_LOCATION},1);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        1);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(
+        int requestCode,
+        String permissions[],
+        int[] grantResults
+    ) {
+        switch (requestCode) {
+            case 1:
+            {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(getActivity(),
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+
+                        _googleMap.setMyLocationEnabled(true);
+                    }
+
+                } else {
+
+                }
+                return;
+            }
+
+        }
     }
 
 
