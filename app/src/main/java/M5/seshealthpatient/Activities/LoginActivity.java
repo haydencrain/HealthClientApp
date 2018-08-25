e1938e9981042b83b9f9a97f112afb8e9497b6ac
Chec98Qu <chec98qu@gmail.com>
2018-08-25 23:03:24 +1000

    Heart rate works

--- app/src/main/java/M5/seshealthpatient/Activities/LoginActivity.java
+++ app/src/main/java/M5/seshealthpatient/Activities/LoginActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import butterknife.BindView;
