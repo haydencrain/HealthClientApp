79ca85373784c13d75ed4ca2726933e94352fd90
Hayden Crain <haydencrain@optusnet.com.au>
2018-09-03 20:02:38 +1000

    add location to data packet, refactor map view

---
+++ app/src/main/java/M5/seshealthpatient/Models/LocationDefaults.java
@@ -0,0 +1,8 @@
+package M5.seshealthpatient.Models;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class LocationDefaults {
+    public static final LatLng mDefaultLocation = new LatLng(-33.86, 151.2);
+    public static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+}
