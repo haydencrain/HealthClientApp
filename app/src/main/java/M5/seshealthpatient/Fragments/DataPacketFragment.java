e02aebb9a46475edc928c493a067c37d161fce63
Hayden Crain <haydencrain@optusnet.com.au>
2018-09-04 17:17:34 +1000

    Update Data packet fragment class, refactor datapacket model, make query text box multiline

--- app/src/main/java/M5/seshealthpatient/Fragments/DataPacketFragment.java
+++ app/src/main/java/M5/seshealthpatient/Fragments/DataPacketFragment.java
@@ -29,8 +29,8 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import M5.seshealthpatient.Models.DataPacket;
 import M5.seshealthpatient.Models.LocationDefaults;
-import M5.seshealthpatient.Models.dataPacket;
 import M5.seshealthpatient.R;
 
 /**
@@ -38,23 +38,23 @@ import M5.seshealthpatient.R;
  */
 public class DataPacketFragment extends Fragment {
 
-
     private FragmentManager manager;
     private FragmentTransaction ft;
-
-
     private boolean mLocationPermissionGranted;
     private FusedLocationProviderClient mFusedLocationProviderClient;
 
-    // heart rate stuff
-    private static Button btnHeartRate = null;
-    private static String heartRate;
 
-    // location
-    private static Button btnLocation;
+    // view components
+    private EditText queryTextBox;
+    private TextView tvHeartRate;
+    private Button btnHeartRate;
+    private Button btnLocation;
     private TextView txtLocation;
-    private Location mLastKnownLocation;
-    private String queryText;
+    private Button btnSendPacket;
+
+    // data packet
+    DataPacket dataPacket;
+
 
     public DataPacketFragment() {
         // Required empty public constructor
@@ -63,54 +63,50 @@ public class DataPacketFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);// Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_data_packet, container, false);
-
-
-        EditText queryBox = (EditText) v.findViewById(R.id.queryBox);
-        queryText = queryBox.getText().toString();
-
-        Button sendPacketbtn = (Button) v.findViewById(R.id.btnSendQ);
-        sendPacketbtn.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                String uid = user.getUid();
-
-                DatabaseReference dbref = FirebaseDatabase.getInstance().getReference("Users/" + uid);
-
-                dataPacket dp = new dataPacket(txtLocation.getText().toString(), heartRate, queryText);
-
-                dbref.child("Queries").child("1").setValue(dp);
-
-                Toast.makeText(getActivity(),
-                        "Query Sent Successfully, try and stay alive", Toast.LENGTH_LONG).show();
-
-
-
-
-            }
-
-
-        });
-
-
-
-
-
-
-
-
-
-
-
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
 
-        txtLocation = v.findViewById(R.id.txtLocation);
-        btnLocation = v.findViewById(R.id.btnLocation);
+        View view = inflater.inflate(R.layout.fragment_data_packet, container, false);
+        bindViewComponents(view);
+
+        dataPacket = new DataPacket();
+
+        Bundle arguments = getArguments();
+        if(arguments != null)
+        {
+            String heartRate = (String)arguments.get("str");
+            dataPacket.setHeartRate(heartRate);
+            tvHeartRate.setText(heartRate);
+        }
+
+        setEventListeners();
+        return view;
+    }
+
+    private void bindViewComponents(View view) {
+        queryTextBox = view.findViewById(R.id.queryBox);
+        txtLocation = view.findViewById(R.id.txtLocation);
+        btnLocation = view.findViewById(R.id.btnLocation);
+        btnHeartRate = view.findViewById(R.id.btnSHR);
+        tvHeartRate = view.findViewById(R.id.tvHeartRate);
+        btnSendPacket = view.findViewById(R.id.btnSendQ);
+    }
+
+    private void setEventListeners() {
+        btnHeartRate.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                manager = getFragmentManager();
+                HeartRateFragment myJDEditFragment = new HeartRateFragment();
+                ft = manager.beginTransaction();
+                ft.replace(R.id.fragment_container, myJDEditFragment);
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+
+        } );
+
         btnLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -119,43 +115,20 @@ public class DataPacketFragment extends Fragment {
             }
         });
 
-        btnHeartRate = v.findViewById( R.id.btnSHR );
+        btnSendPacket.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                String uid = user.getUid();
+                DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("Users/" + uid);
 
-        btnHeartRate.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                dataPacket.setQuery(queryTextBox.getText().toString());
 
-                manager = getFragmentManager();
-                HeartRateFragment myJDEditFragment = new HeartRateFragment();
-                ft = manager.beginTransaction();
-                ft.replace(R.id.fragment_container, myJDEditFragment);
-                ft.addToBackStack(null);
-                ft.commit();
-
+                dbRef.child("Queries").child("1").setValue(dataPacket);
+                Toast.makeText(getActivity(), "Query Sent Successfully, try and stay alive", Toast.LENGTH_LONG).show();
             }
-            
-        } );
 
 
-        TextView tvObj = (TextView)v.findViewById(R.id.fm01tvid);
-
-
-        Bundle arguments = getArguments();
-        if(arguments == null)
-        {
-            heartRate = "";
-            tvObj.setText( heartRate );
-        }
-        else {
-            String x = (String)getArguments().get( "str");
-            tvObj.setText( x );
-
-        }
-
-
-
-
-        return v;
+        });
     }
 
     private void setDeviceLocation() {
@@ -171,11 +144,9 @@ public class DataPacketFragment extends Fragment {
                     public void onComplete(@NonNull Task<Location> task) {
                     if (task.isSuccessful()) {
                         // Set the map's camera position to the current location of the device.
-                        mLastKnownLocation = task.getResult();
-                        txtLocation.setText(mLastKnownLocation.getLatitude() + ", " + mLastKnownLocation.getLongitude());
-
-                    } else {
-                        // can't set location
+                        Location location = task.getResult();
+                        dataPacket.setLocation(location);
+                        txtLocation.setText(String.format("%s, %s", location.getLatitude(), location.getLongitude()));
                     }
                     }
                 });
--- app/src/main/java/M5/seshealthpatient/Models/dataPacket.java
+++ app/src/main/java/M5/seshealthpatient/Models/dataPacket.java
@@ -1,13 +1,66 @@
 package M5.seshealthpatient.Models;
 
-public class dataPacket {
-    String location;
-    String heartRate;
-    String query;
+import android.location.Location;
 
-    public dataPacket(String location, String heartRate, String query) {
-        this.location = location;
+import java.util.LinkedList;
+
+public class DataPacket {
+    private String query;
+    private String heartRate;
+    private Location location;
+    private LinkedList<String> files;
+
+    public DataPacket(String query, String heartRate, Location location) {
         this.heartRate = heartRate;
         this.query = query;
+        this.location = location;
+        this.files = new LinkedList<>();
+    }
+
+    public DataPacket() {
+        this.location = null;
+        this.query = null;
+        this.heartRate = null;
+        this.files = new LinkedList<>();
+    }
+
+    public void addFile(String file) {
+        files.add(file);
+    }
+
+    public String getHeartRate() {
+        return heartRate;
+    }
+
+    public void setHeartRate(String heartRate) {
+        this.heartRate = heartRate;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public double getLongitude() {
+        return location.getLongitude();
+    }
+
+    public double getLatitude() {
+        return location.getLatitude();
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public LinkedList<String> getFiles() {
+        return files;
     }
 }
